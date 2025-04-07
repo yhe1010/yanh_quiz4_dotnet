2f71b65b276a60c664ef043314501237ec165b72
Yanhua He <yhe494@gmail.com>
2025-04-07 12:10:56 -0400

    add the database access message

--- yanh_quiz4_dotnet/Program.cs
+++ yanh_quiz4_dotnet/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Database access has already been done on April 7, 2025 by Yanhua");
